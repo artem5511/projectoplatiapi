8ca6ef8b2d7873cff2c7a156cac5f01a363b6c25
artem5511 <artem_5511@mail.ru>
2022-12-07 16:32:13 +0300

    Updates

---
+++ precache-manifest.67e7d320dc786015c519407af0b53894.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b07699d548b8ed1d2203",
+    "url": "/projectoplatiapi/static/css/main.056d42be.chunk.css"
+  },
+  {
+    "revision": "b07699d548b8ed1d2203",
+    "url": "/projectoplatiapi/static/js/main.f7157aeb.chunk.js"
+  },
+  {
+    "revision": "45aab333af8d18f3537e",
+    "url": "/projectoplatiapi/static/js/runtime~main.978bd158.js"
+  },
+  {
+    "revision": "0891aa6c439980c940aa",
+    "url": "/projectoplatiapi/static/js/2.f6e0b6dc.chunk.js"
+  },
+  {
+    "revision": "59e4468e193d69f80c99",
+    "url": "/projectoplatiapi/static/js/3.c390db1f.chunk.js"
+  },
+  {
+    "revision": "ec46bfeb2926b449ada390fe0b672bba",
+    "url": "/projectoplatiapi/static/media/Sequence diagram for payment.ec46bfeb.jpg"
+  },
+  {
+    "revision": "20acda12f366fd6a34f68a7bb580022c",
+    "url": "/projectoplatiapi/index.html"
+  }
+];
