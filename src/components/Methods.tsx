0335755bf9353063ee8aab476907799e5a25f2aa
artem5511 <artem_5511@mail.ru>
2022-12-07 13:49:13 +0300

    route

--- src/components/Methods.tsx
+++ src/components/Methods.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import Sequense from './image/Sequence diagram for payment.jpg'
 import './Methods.css'
+import Button from '@material-ui/core/Button';
+import {makeStyles} from '@material-ui/core/styles';
 
 export const Methods = () => {
-    return (
+    // function changeSizeImage () {
+    //     return (
+    //     <img alt='' src={Sequense} className={'diagram'}/>
+    //     )
+    // }
+    // console.log(changeSizeImage)
+
+        return (
         <div>
             <h1>Merchant Presented Mode - Dynamic Website QR</h1>
             Сканирование QR-кода мерчанта - Динамический QR на веб-сайте
-            <div className={'diagram'}>
-                <img src={Sequense}/>
-            </div>
             <div>
-                <Button variant="contained">ОПЛАТИТЬ</Button>
+                <img alt='' src={Sequense}/>
+            </div>
+            <br/>
+            <div>
+                <Button variant="contained" color="secondary" size="large">ОПЛАТИТЬ</Button>
             </div>
         </div>
-
     );
 }
