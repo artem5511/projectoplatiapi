538739a03dd51324a1d5f26c25443852527ba287
artem5511 <artem_5511@mail.ru>
2023-05-25 12:01:00 +0300

    Update Get.tsx
    add code Get

--- src/components/requests/Get.tsx
+++ src/components/requests/Get.tsx
@@ -1,1 +1,21 @@
-import React from "react";
+import React from 'react';
+
+const myHeaders = new Headers();
+myHeaders.append("regNum", "OPL000000722");
+myHeaders.append("password", "cloudBY12345");
+myHeaders.append("Content-Type", "application/json");
+
+let requestParam: RequestInit = {
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow',
+    mode: 'cors'
+};
+
+export const Requestget = () => {
+
+    fetch('https://bpay-testcashdesk.lwo.by/ms-pay/pos/payments/38717', requestParam)
+        .then(response => response.text())
+        // .then(data => console.log(data));
+        .then(data => alert(data));
+}
