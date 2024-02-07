611e2c615f1fa738dfb0bfd85ac634b7b18eb928
artem5511 <artem_5511@mail.ru>
2024-02-07 11:22:03 +0300

    add new copmonents

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import {About} from './components/Aboutpage'
 import {Methods} from './components/Methods'
 import {Notfoundpage} from './components/Notfoundpage'
 import {List} from "@material-ui/core";
+import {Gui} from "./components/Gui";
+import {Page1} from "./components/Methodspages/Page1";
 
 
 const App = () => {
@@ -13,17 +15,19 @@ const App = () => {
       <div>
         <div className={'first'}>
           <header className={'hed'}>
-            <NavLink to={"/"}>Home</NavLink>
+            <NavLink to={"/"}>Main</NavLink>
             <NavLink to={"/methods"}>Methods of Payment</NavLink>
-            <NavLink to={"/about"}>Guide UI</NavLink>
+            <NavLink to={"/gui"}>Guide UI</NavLink>
             <NavLink to={"/about"}>About  “Oplati”</NavLink>
           </header>
           <Routes>
             <Route path={"/"} element={<Homepage/>}/>
             {/*<Route path={"/about"} element={<About/>}/>*/}
             <Route path={"/methods"} element={<Methods/>}/>
+            <Route path={"/gui"} element={<Gui/>}/>
             <Route path={"/about"} element={<About/>}/>
             <Route path={"*"} element={<Notfoundpage/>}/>
+            <Route path={"/page1"} element={<Page1/>}/>
           </Routes>
                 {/*<footer className={'foot'}>*/}
                 {/*    <span>logo</span>*/}
