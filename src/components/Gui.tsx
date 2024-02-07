4a18278785f1954af63a74484c0659566e36311a
artem5511 <artem_5511@mail.ru>
2024-02-07 11:22:03 +0300

    add new copmonents

--- src/components/Gui.tsx
+++ src/components/Gui.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
-import './Notfoundpage.css'
+import './Gui.css'
 
-export const Notfoundpage = () => {
+export const Gui = () => {
     const colorAdo = {
         color: 'blue'
     }
     return (
-        <div className={'bl'}>
-            This page doesn't exist. Go <NavLink to="/" style={colorAdo}>Home</NavLink>
-        </div>
+        <>
+            <div>
+                <p>Примеры оформления сайта и текущих статусов стостояния платежа</p>
+            </div>
+            <div className={'bl'}>
+                This page doesn't exist. Go <NavLink to="/" style={colorAdo}>Home</NavLink>
+            </div>
+        </>
+
     );
 }
--- src/components/Methods.tsx
+++ src/components/Methods.tsx
@@ -1,55 +1,13 @@
 import React from 'react';
-import Sequense from './image/Sequence diagram for payment.jpg'
-import webpayments from './image/postpayments.jpg'
-import imgget from './image/getpayments.jpg'
-import './Methods.css'
-import Button from '@material-ui/core/Button';
-import {makeStyles} from '@material-ui/core/styles';
-import {Request} from "./requests/Post";
-import {Requestget} from "./requests/Get";
+import Stages from './Stages';
+import style from "./Stages.module.css";
+
 
 export const Methods = () => {
-    // function changeSizeImage () {
-    //     return (
-    //     <img alt='' src={Sequense} className={'diagram'}/>
-    //     )
-    // }
-    // console.log(changeSizeImage)
-
         return (
-        <div className={"box"}>
-            <div className={"box1"}>
-                <div><h2>Merchant Presented Mode - Dynamic Website QR</h2></div>
-                <div><img className={"imgmain"} alt='' src={Sequense}/></div>
+            <div className={style.hed}>
+             <Stages/>
             </div>
-            <div className={"box2"}>
-                <div>
-                    <div><Button  variant="contained" color="secondary" size="large" onClick={Request}>ОПЛАТИТЬ</Button></div>
-                    <div><img className={'imgpost'} alt='' src={webpayments}/></div>
-                </div>
-                <div>
-                    <div><Button  variant="contained" color="primary" size="large" onClick={Requestget}>ПОЛУЧИТЬ СТАТУС</Button></div>
-                    <div><img className={'imgget'} alt='' src={imgget}/></div>
-                </div>
-
-                <div className="result">result</div>
-            </div>
-            {/*<h1>Merchant Presented Mode - Dynamic Website QR</h1>*/}
-            {/*Сканирование QR-кода мерчанта - Динамический QR на веб-сайте*/}
-            {/*<div>*/}
-            {/*    <img alt='' src={Sequense}/>*/}
-            {/*</div>*/}
-            {/*<br/>*/}
-            {/*<div className={'button_get'}>*/}
-            {/*    <div>*/}
-            {/*        <Button  variant="contained" color="secondary" size="large" onClick={Request}>ОПЛАТИТЬ</Button>*/}
-            {/*    </div>*/}
-            {/*    <div>*/}
-            {/*        <Button  variant="outlined" color="default" size="large" onClick={Requestget}>ПОЛУЧИТЬ СТАТУС</Button>*/}
-            {/*    </div>*/}
-            {/*    <div className="result">result</div>*/}
-            {/*</div>*/}
-        </div>
     );
 }
 
